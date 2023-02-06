16d798e8fe5004033e498dbc934adf93627cb5b5
Cecilia <cecibeuret@gmail.com>
2023-02-05 22:40:04 -0300

    componentes

--- CBportfolio/src/app/app-routing.module.ts
+++ CBportfolio/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'sobre-mi', component: SobreMiComponent},
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'education', component: EducacionComponent},
+  {path: 'proyectos', component: ProyectosComponent},
+  {path: 'habilidades', component: HabilidadesComponent},
+  {path: 'hobbies', component: HobbiesComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: '**', component: Pagina404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
--- CBportfolio/src/app/app.module.ts
+++ CBportfolio/src/app/app.module.ts
@@ -5,12 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
+import { IndexComponent } from './components/index/index.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    LoginComponent,
+    LogoutComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    HobbiesComponent,
+    IndexComponent,
+    Pagina404Component,
+
   ],
   imports: [
     BrowserModule,
